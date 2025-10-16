17258ba75933236fb7f8fe8f0e754c37dece6aa5
D3V1LL-cyber <hiroesmen@gmail.com>
2025-10-16 11:38:12 +0300

    Add files via upload

---
+++ CaesarShipher/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CaesarShipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(117, 34);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(347, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(117, 91);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(347, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(117, 117);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(132, 57);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(332, 117);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(132, 57);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter the text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(567, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Alert!!! - чтобы расшифровать текст, нужно результат зашифровки вставить в первую" +
+    " строку и нажать Decrypt";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(494, 13);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(581, 216);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ CaesarShipher/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaesarShipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Расширяем метод шифрования, учитывая русские символы
+        private string CaesarEncrypt(string input, int shift)
+        {
+            char[] buffer = input.ToCharArray();
+
+            foreach (var ch in buffer)
+            {
+                if (char.IsLetter(ch)) // Проверяем, является ли символ буквой
+                {
+                    bool isUpperCase = char.IsUpper(ch); // Определяем регистр символа
+
+                    // Обрабатываем русский алфавит отдельно
+                    if ((ch >= 'А' && ch <= 'Я') || (ch >= 'а' && ch <= 'я'))
+                    {
+                        int alphabetSize = 32; // Размер русского алфавита
+
+                        // Вычисляем новое положение символа в алфавите
+                        int indexInAlphabet = ch - (isUpperCase ? 'А' : 'а'); // Индекс в алфавите
+                        int shiftedIndex = (indexInAlphabet + shift) % alphabetSize; // Применяем сдвиг
+
+                        // Возвращаемся к символу
+                        buffer[Array.IndexOf(buffer, ch)] = (char)(shiftedIndex + (isUpperCase ? 'А' : 'а'));
+                    }
+                    else
+                    {
+                        // Если символ латиница, применяем стандартный сдвиг
+                        char startChar = isUpperCase ? 'A' : 'a';
+                        int indexInLatinAlph = ch - startChar;
+                        int shiftedLatinIndex = (indexInLatinAlph + shift) % 26;
+                        buffer[Array.IndexOf(buffer, ch)] = (char)(shiftedLatinIndex + startChar);
+                    }
+                }
+            }
+
+            return new string(buffer);
+        }
+
+        // Метод для дешифровки аналогичен шифрованию, только со сдвигом назад
+        private string CaesarDecrypt(string input, int shift)
+        {
+            return CaesarEncrypt(input, 32 - shift); // Используем тот же метод, меняя направление сдвига
+        }
+
+        
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string textToEncrypt = txtInput.Text.Trim();
+                int shiftValue = 3; // Можно настроить любое значение сдвига
+
+                string encryptedText = CaesarEncrypt(textToEncrypt, shiftValue);
+                txtOutput.Text = encryptedText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string textToDecrypt = txtInput.Text.Trim();
+                int shiftValue = 3; // Значение сдвига должно соответствовать использованному при шифровании
+
+                string decryptedText = CaesarDecrypt(textToDecrypt, shiftValue);
+                txtOutput.Text = decryptedText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();//закрытие формы
+        }
+    }
+}
